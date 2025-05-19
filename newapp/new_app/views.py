81739b5cf76794b46e4755c898af3fb0e824e05b
UlianaAsadulina <u.asadulina@icloud.com>
2025-05-19 14:51:36 -0400

    add html templates

--- newapp/new_app/views.py
+++ newapp/new_app/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 
 def home(response):
-    return HttpResponse("Home Page")
+    return render(response, "new_app/home.html", {})
 
 def index(response, id):
     current_list = ToDoList.objects.get(id=id)
-    items = current_list.item_set.get(id=1)
-    return HttpResponse("<h1> %s </h1><p>%s</p> " %(current_list.name, str(items.text)) )
+    # items = current_list.item_set.get(id=1)
+    return render(response, "new_app/list.html", {"current_list": current_list})
